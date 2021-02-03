4fa626078cda04f50dbca64e7bfa87fd8b02184a
maks <zenzelyuk.m@gmail.com>
2021-02-03 21:55:02 +0200

    Changes

--- src/main/java/ua/com/elcentr/Application.java
+++ src/main/java/ua/com/elcentr/Application.java
@@ -1,6 +1,10 @@
 package ua.com.elcentr;
 
 
+import ua.com.elcentr.dao.CustomerDAO;
+import ua.com.elcentr.dao.UserDAO;
+import ua.com.elcentr.model.Customer;
+
 import java.util.logging.Logger;
 
 public class Application {
@@ -8,7 +12,7 @@ public class Application {
     private static final Logger LOG = Logger.getLogger(Application.class.getName());
 
     public static void main(String[] args) {
-
-
+        CustomerDAO.save("aass", "notessss");
+        UserDAO.getUserByLoginAndPassword("adajsd", "adasd");
     }
 }
